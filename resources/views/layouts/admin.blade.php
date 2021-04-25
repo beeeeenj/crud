a7b5b3a09a26c28a05ec19df168f0ff6f5ef9bf6
Benjamin Gonzales <bench.gonzales1203@gmail.com>
2021-04-25 20:25:36 +0800

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Lindela Travel & Tours</title>
+  <title>Travel & Tours</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <!-- Google Font: Source Sans Pro -->
